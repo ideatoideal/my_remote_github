35dfbe650022b9ccb663cf464f973595d2048583
frendy <1428648606@qq.com>
2018-03-16 17:58:33 +0800

    20180316 finance

--- pycode/project1/frendy_tool.py
+++ pycode/project1/frendy_tool.py
@@ -1,6 +1,18 @@
-def get_insert_sql(cursor,tbname):
-    sql = "select COLUMN_NAME from information_schema.COLUMNS where table_name = '"+tbname+"'";
+def get_insert_sql(cursor, tbname):
+    sql = "select COLUMN_NAME from information_schema.COLUMNS where table_name = '" + tbname + "'";
     cursor.execute(sql)
-    #http://blog.csdn.net/guofeng93/article/details/53994112
+    # http://blog.csdn.net/guofeng93/article/details/53994112
     data = cursor.fetchall()
-    print(data)
+    data = list(data)
+    columns = ""
+    columns = []
+    values = []
+    for row in data:
+        columns.append(row[0])
+        values.append("%s")
+    columns = ",".join(columns)
+    values = ",".join(values)
+    # print(data);
+    sql = "insert ignore " + tbname + "(" + columns + ") values(" + values + ")"
+    print(sql)
+    return sql
--- pycode/project1/test.py
+++ pycode/project1/test.py
@@ -1,20 +1,82 @@
-import tushare as ts
+import tushare
 import pymysql
+import numpy
 import frendy_tool
+from warnings import filterwarnings
+
+filterwarnings('ignore', category=pymysql.Warning)  # 去掉warning信息
+
+def transform_type(str):
+    types = {
+        "float":"float",
+        "int":"int",
+        "datetime": "datetime",
+        "date":"date",
+        "":"乜嘢都无"
+    }
+    for key in types.keys():
+        if key in str:
+            return types.get(key)
+
+def save_data(conn, data, table_name, primary_key):
+    cursor = conn.cursor()
+    # 查询表格是否存在
+    sql = "SELECT count(1) FROM information_schema.TABLES WHERE table_name ='" + table_name + "';"
+    cursor.execute(sql)
+    ret = cursor.fetchall()
+    name_list = list(data)
+    type_list = data.dtypes
+    columns = []
+    columns.append(data.index.name + " " + transform_type(str(data.index.dtype)))
+    for i in range(len(name_list)):
+        columns.append(name_list[i] + " " + transform_type(str(type_list[i])))
+    columns = ",".join(columns)
+    has_table = True
+    for r in ret:
+        has_table = True if (r[0] != 0) else False
+    if not has_table:
+        sql = "create table " + table_name + "(" + columns
+        if primary_key is not None:
+            sql += " ,primary key(" + primary_key + ")"
+        sql += ") "
+        print(sql);
+    cursor.close()
 
 
+conn = pymysql.connect("localhost", "test", "199104", "testdb")
+cursor = conn.cursor()
+sql = frendy_tool.get_insert_sql(cursor, "day_data")
 
-db = pymysql.connect("localhost","test","199104","testdb")
-cursor = db.cursor()
-frendy_tool.get_insert_sql(cursor,"day_data")
+# 接口返回DataFrame类型 详情百度panda库
+# get_today_all 获取今日数据
+# get_today_ticks 获取当前粉笔记录
+# get_tick_data 分笔记录
+# get_h_data 前复权
+# get_sina_dd 大单 默认大于400手
 
-db.close()
+share_data = tushare.get_h_data('000001',start='2018-03-02',index=True)
 
-#data = ts.get_k_data('000001',start='2011-01-02',index=True)
-#data = ts.get_today_all()
-#print(data)
+#share_data = tushare.get_stock_basics()
+save_data(conn=conn,data=share_data,table_name="hello",primary_key="date")
 
-#查找表 若无则上网取数插入本地表
+print(share_data)
+#print(share_data)
+#share_list = numpy.array(share_data).tolist()
 
-#select COLUMN_NAME from information_schema.COLUMNS where table_name = 'd';
+try:
+    str = None
+    # cursor.executemany(sql,share_list)
+except Exception as e:
+    print("执行MySQL: % s时出错： % s" % (sql, e))
+finally:
+    cursor.close()
+    conn.commit()
+    conn.close()
 
+# data = ts.get_k_data('000001',start='2011-01-02',index=True)
+# data = ts.get_today_all()
+# print(data)
+
+# 查找表 若无则上网取数插入本地表
+
+# select COLUMN_NAME from information_schema.COLUMNS where table_name = 'd';
