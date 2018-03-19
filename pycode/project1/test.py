2f73386bdba58db989854e2efd62f593d5f2c1f1
frendy <1428648606@qq.com>
2018-03-19 17:54:42 +0800

    20180319 finance

--- pycode/project1/test.py
+++ pycode/project1/test.py
@@ -1,89 +1,35 @@
 import tushare
 import pymysql
 import numpy
-import frendy_tool
+import share_tool
+import util
 from warnings import filterwarnings
-
 filterwarnings('ignore', category=pymysql.Warning)  # 去掉warning信息
 
 
-def transform_type(str):
-    types = {
-        "float": "double",
-        "int": "bigint",
-        "datetime": "datetime",
-        "date": "datetime",
-        "": "乜嘢都无"
-    }
-    for key in types.keys():
-        if key in str:
-            return types.get(key)
 
-
-def save_data(conn, data, table_name, primary_key):
-    cursor = conn.cursor()
-    # 查询表格是否存在
-    sql = "SELECT count(1) FROM information_schema.TABLES WHERE table_name ='" + table_name + "';"
-    cursor.execute(sql)
-    ret = cursor.fetchall()
-    name_list = list(data)
-    type_list = data.dtypes
-    columns = []
-    columns.append(data.index.name + " " + transform_type(str(data.index.dtype)))
-    for i in range(len(name_list)):
-        columns.append(name_list[i] + " " + transform_type(str(type_list[i])))
-    columns = ",".join(columns)
-    has_table = True
-    for r in ret:
-        has_table = True if (r[0] != 0) else False
-    if not has_table:  # 建表
-        sql = "create table " + table_name + "(" + columns
-        if primary_key is not None:
-            sql += " ,primary key(" + primary_key + ")"
-        sql += ") "
-        print(sql)
-        cursor.execute(sql)
-    # 不重复插入
-    columns = []
-    values = []
-    columns.append(data.index.name)
-    values.append("%s")
-    for i in range(len(name_list)):
-        values.append("%s")
-        columns.append(name_list[i])
-    columns = ",".join(columns)
-    values = ",".join(values)
-    sql = "insert ignore " + table_name + "(" + columns + ") values(" + values + ")"
-    print(sql)
-    data_list = data.values
-    index_list = data.index.values
-    merge_list = []
-    for i in range(len(data_list)):
-        tmp_row = []
-        tmp_row.append(str(index_list[i]))
-        for j in range(len(data_list[i])):
-            tmp_row.append(str(data_list[i][j]))
-        merge_list.append(tmp_row)
-    #data_list = numpy.concatenate( ( numpy.array(data.index)[:,None],numpy.array(data) ),axis=1 )
-    cursor.executemany(sql,merge_list)
-    cursor.close()
-
-
-conn = pymysql.connect("localhost", "test", "199104", "testdb")
+conn = pymysql.connect("localhost", "test", "199104", "testdb", use_unicode=True, charset="utf8")  # 默认utf-8 不然插入可能会出错
 cursor = conn.cursor()
-sql = frendy_tool.get_insert_sql(cursor, "day_data")
 
 # 接口返回DataFrame类型 详情百度panda库
 # get_today_all 获取今日数据
-# get_today_ticks 获取当前粉笔记录
+# get_today_ticks 获取当前分笔记录
 # get_tick_data 分笔记录
-# get_h_data 前复权
-# get_sina_dd 大单 默认大于400手
+# get_h_data 默认qfq
+# get_sina_dd 大单 默认大于400
 
-share_data = tushare.get_h_data('000001', start='2018-02-27', index=True)
 
-# share_data = tushare.get_stock_basics()
-save_data(conn=conn, data=share_data, table_name="hello", primary_key="date")
+# 基本 TODO及时更新数据
+#share_data = tushare.get_stock_basics()
+#util.save_data(conn=conn, data=share_data, table_name="stock_basics", primary_key="code")
+
+
+
+
+#share_data = tushare.get_k_data('000001', start='2011-02-27', index=True,)
+# 日获取
+#  share_data = share_tool.get_h_data('000001', start='2018-02-27', index=True)
+# util.save_data(conn=conn, data=share_data, table_name="hello", primary_key="date,code,autype")
 
 print(share_data)
 # print(share_data)
